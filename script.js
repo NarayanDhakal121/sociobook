7f27bbfe0c319160db32cf83f51ba6f9cab87a4d
NarayanDhakal121 <www.narine121@gmail.com>
2025-01-20 23:43:46 +0545

    info commit

--- script.js
+++ script.js
@@ -6,4 +6,28 @@ function settingMenuToggle() {
 
 darkBtn.onclick = function () {
   darkBtn.classList.toggle("dark-btn-on");
+  document.body.classList.toggle("dark-theme");
+  //  update the  local storage whenever the user changes the state ie dark mode or light mode
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
 };
+
+// localStorage.setItem("theme", "light");
+// localStorage.getItem("theme");
+
+// to store dark mode  and light mode on browser
+
+// first we have to check if the local, storage is present on not in browser
+
+if (localStorage.getItem("theme") == "light") {
+  darkBtn.classList.remove("dark-btn-on");
+  document.body.classList.remove("dark-theme");
+} else if (localStorage.getItem("theme") == "dark") {
+  darkBtn.classList.add("dark-btn-on");
+  document.body.classList.add("dark-theme");
+} else {
+  localStorage.setItem("theme", "light");
+}
